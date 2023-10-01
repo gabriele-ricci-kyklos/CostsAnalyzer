3a9ea899a5d87c54d2c7b1035cc4b89ae9ff9da2
Gabriele Ricci <gabriele.ricci@remira.com>
2023-10-01 17:09:31 +0200

    tests in cmd project

--- src/CostsAnalyzer.Cmd/Program.cs
+++ src/CostsAnalyzer.Cmd/Program.cs
@@ -1,15 +1,77 @@
-﻿using iText.Kernel.Pdf;
-using iText.Kernel.Pdf.Canvas.Parser;
-using iText.Kernel.Pdf.Canvas.Parser.Listener;
-using System.Globalization;
+﻿using CostsAnalyzer.Business.Categories;
+using CostsAnalyzer.Data;
+
+EncryptedDao dao = new(new(@"C:\Temp\CostsAnalyzer\data"));
+CategoryManager categoryManager = new(dao);
+
+var matches =
+    await categoryManager
+        .MatchCategoryAsync("sta minchia marina")
+        .ConfigureAwait(false);
+
+Console.ReadLine();
+
+//StopWord[] stopWords =
+//    File
+//        .ReadAllLines(@"C:\Users\gabriele.ricci\Downloads\italian.txt")
+//        .Select(x => new StopWord() { Word = x })
+//        .ToArray();
+
+//await dao.SaveDataAsync(stopWords);
+
+//List<CategoryWord> data = new();
+//foreach(string file in Directory.GetFiles(@"C:\Temp\CostsAnalyzer\categories_data"))
+//{
+//    Category category = Enum.Parse<Category>(Path.GetFileNameWithoutExtension(file));
+//    string[] lines = File.ReadAllLines(file);
+//    foreach(string line in lines)
+//    {
+//        string[] words = line.Split(' ');
+//        data.AddRange(words.Select(x => new CategoryWord() { Word = x, Category = category }));
+//    }
+//}
+
+//await dao.SaveDataAsync(data.ToArray());
+
+//Console.ReadLine();
 
 
+//using OfficeOpenXml;
 
-public class RawMovementRow
-{
-    public DateTime Date { get; set; }
-    public string Recipient { get; set; }
-    public string Description { get; set; }
-    public decimal Amount { get; set; }
-    public string Currency { get; set; }
-}
+//ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+//using ExcelPackage excelPackage = new(@"C:\Users\gabriele.ricci\Downloads\lista_completa_24092023.xlsx");
+//if (excelPackage.Workbook.Worksheets.Count < 1)
+//{
+//    //error
+//}
+
+//using ExcelWorksheet worksheet = excelPackage.Workbook.Worksheets.First();
+//int row = 20;
+//int col = 1;
+//int colCount = 8;
+//Dictionary<string, List<string>> results = new();
+
+//do
+//{
+//    string name = worksheet.Cells[row, 2].Value.ToString() ?? string.Empty;
+//    string category = worksheet.Cells[row, 6].Value.ToString() ?? string.Empty;
+
+//    if (results.ContainsKey(category))
+//    {
+//        results[category].Add(name);
+//    }
+//    else
+//    {
+//        results.Add(category, new() { name });
+//    }
+//}
+//while (!string.IsNullOrWhiteSpace(worksheet.Cells[++row, col].Value?.ToString()));
+
+//foreach(string key in results.Keys)
+//{
+//    var data = results[key].Distinct().ToList();
+//    File.WriteAllLines(@$"C:\temp\costsanalyzer\{key}.txt", data);
+//}
+
+//bool x = true;
